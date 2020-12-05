726b42970c39b65eb2470e2c80e64a108ee142c8
Loic Tiemani <loictiems@github.com>
2020-12-05 18:54:49 -0500

    API developement

--- app.py
+++ app.py
@@ -1,54 +1,45 @@
-/
+#1. Import Flask
+from flask import Flask, jsonify
+from sqlalchemy import create_engine, func
+from sqlalchemy.ext.automap import automap_base
+from sqlalchemy.orm import Session
 
+import numpy as np
+import pandas as pd
+import datetime as dt
+engine = create_engine("sqlite:///Resources/hawaii.sqlite")
+Base = automap_base()
+Base.prepare(engine, reflect=True)
+Measurement = Base.classes.measurement
+Station = Base.classes.station
+session = Session(engine)
+#2. Create an app
+app = Flask(__name__)
 
-Home page.
+#3.Define Static routes
+@app.route("/")
+def welcome():
+    return (
+        f"Available Routes:<br/>"
+        f"Precipitation:/api/v1.0/precipitation<br/>"
+        f"List of Stations:/api/v1.0/stations<br/>"
+        f"Temperature:/api/v1.0/tobs<br/>"
+        f"Temperature from the start date:/api/v1.0/<start><br/>"
+        f"Temperature from start to end date:/api/v1.0/<start>/<end>"
+    )
 
+@app.route("/api/v1.0/precipitation")
+def precipitation():
+ 
 
-List all routes that are available.
+    last_date = session.query(Measurement.date).order_by(Measurement.date.desc()).first()
+    one_year = dt.timedelta(365)
+    one_year_date = dt.datetime.strptime(last_date[0],"%Y-%m-%d") - one_year
+    precipitation_data = session.query(Measurement.date, Measurement.prcp).\
+        filter(Measurement.date>one_year_date).order_by(Measurement.date).all()
 
-
-
-
-/api/v1.0/precipitation
-
-
-Convert the query results to a dictionary using date as the key and prcp as the value.
-
-
-Return the JSON representation of your dictionary.
-
-
-
-
-/api/v1.0/stations
-
-Return a JSON list of stations from the dataset.
-
-
-
-/api/v1.0/tobs
-
-
-Query the dates and temperature observations of the most active station for the last year of data.
-
-
-Return a JSON list of temperature observations (TOBS) for the previous year.
-
-
-
-
-/api/v1.0/<start> and /api/v1.0/<start>/<end>
-
-
-Return a JSON list of the minimum temperature, the average temperature, and the max temperature for a given start or start-end range.
-
-
-When given the start only, calculate TMIN, TAVG, and TMAX for all dates greater than and equal to the start date.
-
-
-When given the start and the end date, calculate the TMIN, TAVG, and TMAX for dates between the start and end date inclusive.
+    print(precipitation_data)
 
 
 
-
-
+#@app.route("/jsonify")    
